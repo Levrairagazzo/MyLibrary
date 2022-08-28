9f62fb20f9dcf0b4b8f85a9e03f3b2f524e1196e
Sydney Goeury <goeurysydney@gmail.com>
2022-08-28 13:27:37 -0700

    basic UI + add a book and delete button

--- library.js
+++ library.js
@@ -21,13 +21,26 @@ let myBook = new Book("1984", "George Orwell", 99);
 let myBook2 = new Book("Animal Farm", "George Orwell", 99);
 myLibrary.push(myBook, myBook2);
 
+function validateForm() {
+    let formTitle = document.forms["myForm"]["bookTitle"].value;
+    let formAuthor = document.forms["myForm"]["bookAuthor"].value;
+    if (formAuthor === "" || formTitle === "") {
+      alert("Make sure everything is filled out.");
+      return false;
+    }
+    else{
+    addBookToLibrary();
+    return true;
+    }
+  }
+
 
 function addBookToLibrary() {
+
     let newTitle = document.getElementById('title');
     let newAuthor = document.getElementById('author');
     let newPages = document.getElementById('pages');
     console.log(newPages.value);
-
     let newBook = new Book(newTitle.value, newAuthor.value, newPages.value);
     myLibrary.push(newBook);
     newTitle.value = "";
@@ -37,6 +50,17 @@ function addBookToLibrary() {
     displayBook(newBook);
     
   
+}
+
+function checkForm(){
+    let newTitle = document.getElementById('title');
+    let newAuthor = document.getElementById('author');
+    let newPages = document.getElementById('pages');
+
+    if(newTitle.value === '' || newAuthor.value === ''||newPages.value === '')
+        return false;
+    else
+        return true;
 }
 
 // function displayLibrary(){
@@ -54,8 +78,33 @@ function addBookToLibrary() {
 // }
 
 function displayBook(myBook){
-    let ul = document.getElementById('books');
-    let li = document.createElement('li');
-    li.innerHTML = myBook.bookCard;
-    ul.appendChild(li);
+    let bookshelf = document.getElementById('books');
+    let book = document.createElement('div');
+    let titlee = document.createElement('p');
+    let author = document.createElement('p');
+    let numPages = document.createElement('p');
+    let deleteButton = document.createElement('button');
+    
+
+
+    deleteButton.textContent = "Delete Book";
+    deleteButton.addEventListener("click", function() {
+       book.remove();
+      });
+    titlee.innerHTML = myBook.title;
+    author.innerHTML = myBook.author;
+    numPages.innerHTML = myBook.page + " pages";
+
+
+    
+    bookshelf.appendChild(book);
+    book.appendChild(titlee);
+    book.appendChild(author);
+    book.appendChild(numPages);
+    book.appendChild(deleteButton);
 }
+
+
+const testBook = new Book("The Adventures of Tom Sawyer", "Mark Twain", 250);
+
+displayBook(testBook);
